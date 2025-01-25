a9c7b27d2d029d0dffcfb3a62466ccf8727eb0d0
Sabyrkul <osmokarim@gmail.com>
2025-01-25 22:22:28 +0600

    abcd

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Category, Publication
 from .serializers import CategorySerializer, PublicationSerializer
-
 from .permissions import IsOwnerOrReadOnly
+from .filters import CategoryFilter, PublicationFilter
 
 class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CategoryFilter
 
 class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
@@ -17,6 +20,8 @@ class PublicationListCreateAPIView(generics.ListCreateAPIView):
     queryset = Publication.objects.filter(is_archived=False)
     serializer_class = PublicationSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = PublicationFilter
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
